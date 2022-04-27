840c61d6e9a46963003ea92e31aa5704276b43bb
Thelgaris <alex.gabarron84@gmail.com>
2022-04-27 19:31:04 +0000

    FKissues

--- src/models.py
+++ src/models.py
@@ -16,48 +16,32 @@ class User(Base):
     lastname = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False, unique=True)
 
-class Characters(Base):
-    __tablename__ = 'characters'
+class Onecharacter(Base):
+    __tablename__ = 'onecharacter'
     id = Column(Integer, primary_key=True)
-    characters_id = Column(Integer, ForeignKey('characters.id'))
-    onecharacter = relationship(Onecharacter)  
-
-
-
-class Planets(Base):
-    __tablename__ = 'planets'
-    id = Column(Integer, primary_key=True)
-    planets_id = Column(Integer, ForeignKey('oneplanet.id'))
-    oneplanet = relationship(Oneplanet) 
-
-class OnePlanet(Base):
-    __tablename__= 'oneplanet'
-    id = Column(Integer, primary_key=True)
-    oneplanet_props = Column(string(250), Integer, string(250))
-
-class Vehicles(Base):
-    __tablename__ = 'vehicles'
-    id = Column(Integer, primary_key=True)
-    vehicles_id = Column(Integer, ForeignKey('onevehicle.id'))
-    onevehicle = relationship(Onevehicle)
-
+    character_name = Column(String(250), nullable=False, unique=True)
+    character_props = Column(String(250), nullable=False)
+   
+    
 class Onevehicle(Base):
-    __tablename__= 'onevehicle'
+    __tablename__ = 'onevehicle'
     id = Column(Integer, primary_key=True)
-    onevehicle_props = Column(string(250), Integer, string(250))
+    vehicle_name = Column(String(250), nullable=False, unique=True)
+    vehicle_props = Column(String(250), nullable=False)
+    
+class Oneplanet(Base):
+    __tablename__ = 'oneplanet'
+    id = Column(Integer, primary_key=True)
+    planet_name = Column(String(250), nullable=False, unique=True)
+    planet_props = Column(String(250), nullable=False)
 
 class Favourites(Base):
     __tablename__ = 'favourites'
     id = Column(Integer, primary_key=True)
-    characters_id = Column(Integer, ForeignKey('characters.id'), unique=True)
-    characters = relationship(Characters)
-    planets_id = Column(Integer, ForeignKey('planets.id'), unique=True)
-    planets = relationship(Planets)
-    vehicles_id = Column(Integer, ForeignKey('vehicles.id'), unique=True)
-    vehicles = relationship(Vehicles)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-
+    character_name = Column(String(250), unique=True)
+    vehicle_name = Column(String(250), unique=True)
+    planet_name = Column(String(250), nullable=False, unique=True, ForeignKey('oneplanet.planet_name'))
+    oneplanet = relationship(Oneplanet)
 
     def to_dict(self):
         return {}
