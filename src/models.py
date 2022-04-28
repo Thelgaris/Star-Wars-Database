6b2bb32eecafa63ac3c9513b53e91d66464937f8
Thelgaris <alex.gabarron84@gmail.com>
2022-04-28 09:29:54 +0000

    done?¿

--- src/models.py
+++ src/models.py
@@ -8,21 +8,21 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'user'
-    id = Column(Integer, primary_key=True)
-    username = Column(String(250), nullable=False, unique=True)
-    name = Column(String(250), nullable=False)
-    lastname = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False, unique=True)
+
+    
 
 class Onecharacter(Base):
     __tablename__ = 'onecharacter'
     id = Column(Integer, primary_key=True)
     character_name = Column(String(250), nullable=False, unique=True)
     character_props = Column(String(250), nullable=False)
-   
-    
+
+class Allcharacters(Base):
+    __tablename__ = 'allcharacters' 
+    id = Column(Integer, primary_key=True)
+    character_id = Column(Integer, ForeignKey('onecharacter.id'))
+    character = relationship(Onecharacter)
+
 class Onevehicle(Base):
     __tablename__ = 'onevehicle'
     id = Column(Integer, primary_key=True)
@@ -38,13 +38,25 @@ class Oneplanet(Base):
 class Favourites(Base):
     __tablename__ = 'favourites'
     id = Column(Integer, primary_key=True)
-    character_name = Column(String(250), unique=True)
-    vehicle_name = Column(String(250), unique=True)
-    planet_name = Column(String(250), nullable=False, unique=True, ForeignKey('oneplanet.planet_name'))
+    character_id = Column(Integer, ForeignKey('onecharacter.id'))
+    onecharacter = relationship(Onecharacter)
+    vehicle_id = Column(Integer, ForeignKey('onevehicle.id'))
+    onevehicle = relationship(Onevehicle)
+    planet_id = Column(Integer, ForeignKey('oneplanet.id'))
     oneplanet = relationship(Oneplanet)
 
     def to_dict(self):
         return {}
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False, unique=True)
+    name = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
+    favourites_id = Column(Integer, ForeignKey('favourites.id'))
+    favourites = relationship(Favourites)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
